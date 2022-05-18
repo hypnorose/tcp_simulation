f7f16b6b6ec68465c8ef19b69483625c02da6923
hypnorose <yossarian2298@gmail.com>
2022-05-18 21:23:29 +0200

    wip

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
-
-
+#include "h/core.h"
+#include <iostream>
 int main(){
-    
+    char z;
+    Core core(4,1000,100);
+    core.printInfo();
+    cin >> z;
+    for(int i = 0 ; i < 100; i ++ ){
+        core.step();
+        core.printInfo();
+        getc(stdin);
+    }
 }
--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,14 +1,25 @@
 #include <gtest/gtest.h>
 #include "../h/core.h"
+#include "../h/packet.h"
+#include <vector>
 #ifdef __STRICT_ANSI__
 #undef __STRICT_ANSI__
 #endif
 
-TEST(basicStructure,coreAndNodes){
+TEST(basicStructure,dstCheck){
     Core core(6,1000,100);
     EXPECT_EQ(core.nodes[0].dst_id,3);
     EXPECT_EQ(core.nodes[3].dst_id,0);
     EXPECT_EQ(core.nodes[4].dst_id,1);
     EXPECT_EQ(core.nodes[5].dst_id,2);
     EXPECT_EQ(core.nodes[2].dst_id,5);
+}
+
+TEST(basicStructure,sendCheck){
+    Core core(6,1000,100);
+    vector<Packet> rcvPackets = core.nodes[0].snd(0);
+    Packet rcvdAck = core.nodes[1].rcv(rcvPackets[0],1);
+    core.nodes[0].rcv(rcvdAck,1);
+    EXPECT_EQ(core.nodes[0].cwnd,2);
+    core.nodes[0].rcv(rcvdAck,1);
 }
