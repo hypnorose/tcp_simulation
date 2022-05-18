dea7f6f064cf97bf6b7f23f414466c5209217f08
hypnorose <yossarian2298@gmail.com>
2022-05-18 13:33:23 +0200

    wip

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,15 +1,14 @@
 #include <gtest/gtest.h>
+#include "../h/core.h"
 #ifdef __STRICT_ANSI__
 #undef __STRICT_ANSI__
 #endif
 
-TEST(test1,BasicAssertions){
-
-    EXPECT_EQ(3*4,12);
-    EXPECT_EQ(2+2*2,6);
-}
-TEST(test1,BasicAssertions2){
-
-    EXPECT_EQ(3*4,12);
-    EXPECT_EQ(2+2*2+1,7);
+TEST(basicStructure,coreAndNodes){
+    Core core(6,1000,100);
+    EXPECT_EQ(core.nodes[0].dst_id,3);
+    EXPECT_EQ(core.nodes[3].dst_id,0);
+    EXPECT_EQ(core.nodes[4].dst_id,1);
+    EXPECT_EQ(core.nodes[5].dst_id,2);
+    EXPECT_EQ(core.nodes[2].dst_id,5);
 }
