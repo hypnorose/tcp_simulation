8223b678d27cc90bedb50b469cc0d4536abe6825
hypnorose <yossarian2298@gmail.com>
2022-05-20 13:32:04 +0200

    wip

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include "h/core.h"
 #include <iostream>
-int main(){
-    double beta;
-    int nodesNumber,queueSize,speed,alpha,timeout;
+
+void readInput(int &nodesNumber,int &queueSize, int &speed,
+    int &alpha, double &beta, int& timeout){
     cout << "insert nodes number:" << endl;
     cin >> nodesNumber;
     cout << "insert max packets in core:" << endl;
@@ -16,7 +16,23 @@ int main(){
     cout << "insert number of units node can wait for ACK:" << endl;
     cin >> timeout;
 
+}
+
+int main(){
+    double beta;
+    int nodesNumber,queueSize,speed,alpha,timeout;
+    nodesNumber = 10;
+    queueSize = 1000;
+    speed = 50;
+    alpha = 1;
+    beta = 0.5;
+    timeout = 3;
     
+    char z;
+    cout << "read custom config? y/n: " << endl;
+    cin >> z;
+    if(z == 'y')readInput(nodesNumber,queueSize,speed,
+        alpha,beta,timeout);
 
     Core core(nodesNumber,queueSize,speed);
     core.setAlpha(alpha);
